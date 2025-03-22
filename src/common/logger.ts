b856f25b93051d89cb88cefba7b68c4f47e2c83f
richard-nagy <47828652+richard-nagy@users.noreply.github.com>
2025-03-22 23:13:52 +0100

    #17: Improve console logs

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,24 +1,26 @@
+import { getTimestamp } from "./functions";
+
 const isDev = process.env.NODE_ENV === "development";
 
 export const Logger = {
     log: (...args: unknown[]) => {
         if (isDev) {
-            console.log(args);
+            console.log(`[${getTimestamp()}]:`, ...args);
         }
     },
     warn: (...args: unknown[]) => {
         if (isDev) {
-            console.warn(args);
+            console.warn(`[${getTimestamp()}]:`, ...args);
         }
     },
     error: (...args: unknown[]) => {
         if (isDev) {
-            console.error(...args);
+            console.error(`[${getTimestamp()}]:`, ...args);
         }
     },
     debug: (...args: unknown[]) => {
         if (isDev) {
-            console.debug(...args);
+            console.debug(`[${getTimestamp()}]:`, ...args);
         }
     },
 };
